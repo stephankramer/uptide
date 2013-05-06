a796fa02c4c15a92bde66278407aa74ea2d2c0c5
Stephan Kramer <s.kramer@imperial.ac.uk>
2013-05-06 19:00:33 +0100

    Adding some files.

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='uptide',
+      version='0.1',
+      author='Stephan Kramer',
+      author_email='s.kramer@imperial.ac.uk',
+      url='http://www.opentidalfarm.com',
+      packages = ['uptide'] 
+      )
