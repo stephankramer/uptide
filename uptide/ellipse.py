df289e80b2a5b3571743c1683bff05a3b75a0aa6
Stephan Kramer <s.kramer@imperial.ac.uk>
2013-06-05 17:43:26 +0100

    tidal_ellipse_parameters() to accept numpy arrays.

--- uptide/ellipse.py
+++ uptide/ellipse.py
@@ -1,4 +1,4 @@
-import math
+import numpy
 
 def compute_focus_squared(au,pu,av,pv):
   """Computes the square of the focus of the ellipse from the
@@ -7,16 +7,18 @@ def compute_focus_squared(au,pu,av,pv):
   ellipse. If a and b are the major and minor radii, then this returns:
 
      f**2 = a**2 - b**2"""
-  return math.sqrt(au**4+av**4+2*au**2*av**2*math.cos(2*(pu-pv)))
+  return numpy.sqrt(au**4+av**4+2*au**2*av**2*numpy.cos(2*(pu-pv)))
 
 def tidal_ellipse_parameters(au,pu,av,pv):
   """Computes the length, width and radius of a tidal ellipse
-  from the amplitude and phase of the x and y components of velocity."""
+  from the amplitude and phase of the x and y components of velocity.
+  Accepts numpy arrays to compute parameters for multiple consituents
+  at once."""
   f2 = compute_focus_squared(au,pu,av,pv)
-  a = math.sqrt(0.5*(au**2+av**2+f2))
-  b = math.sqrt(0.5*(au**2+av**2-f2))
+  a = numpy.sqrt(0.5*(au**2+av**2+f2))
+  b = numpy.sqrt(0.5*(au**2+av**2-f2))
   # formula A3:4a from Pugh
-  delta = 0.5*math.atan2(av**2*math.sin(2*(pu-pv)),au**2+av**2*math.cos(2*(pu-pv)))
+  delta = 0.5*numpy.arctan2(av**2*numpy.sin(2*(pu-pv)),au**2+av**2*numpy.cos(2*(pu-pv)))
   # formula A3:8 from Pugh
-  theta = math.atan2(av*math.cos(pu-pv-delta), au*math.cos(delta))
+  theta = numpy.arctan2(av*numpy.cos(pu-pv-delta), au*numpy.cos(delta))
   return a, b, theta
