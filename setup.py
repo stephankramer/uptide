b76669c01b4e080d73c3dda39d263e7506f24be9
Stephan Kramer <s.kramer@imperial.ac.uk>
2014-01-03 15:26:12 +0000

    Wrapping libfes using cython, so we can call it directly from python.
    This adds a uptide.fes2012 module with a Fes class that implements the core functionality
    of the libfes library. The libfes library has to be build with -fPIC:
    
      CFLAGS=-fPIC -O2 ./configure && make
    
    To install uptide with direct uptide support:
    
      FES_DIR=<path-to-fes-src> python setup.py install --with-fes --prefix=$(HOME)
    
    NOTE: that this only recomplies fes2012.c it does not rerun cython (to avoid
    adding a cython dependency). To regenerate fes2012.c from fes2012.pyx, run:
    
      cython fes2012.pyx

--- setup.py
+++ setup.py
@@ -1,8 +1,32 @@
 from distutils.core import setup
+from distutils.extension import Extension
+import os
+import sys
+
+ext_modules = []
+script_args = sys.argv[1:]
+if '--with-fes' in script_args:
+  script_args.remove('--with-fes')
+  library_dirs = []; include_dirs = []
+  if 'FES_DIR' in os.environ:
+    library_dirs.append(os.path.join(os.environ['FES_DIR'], 'src'))
+    include_dirs.append(os.path.join(os.environ['FES_DIR'], 'src'))
+  if 'FES_LIB_DIR' in os.environ:
+    library_dirs.append(os.environ['FES_LIB_DIR'])
+  if 'FES_INCLUDE_DIR' in os.environ:
+    include_dirs.append(os.environ['FES_INCLUDE_DIR'])
+  ext_modules.append(Extension("fes2012", ["uptide/fes2012.c"],
+    libraries=["fes", "netcdf"],
+    library_dirs=library_dirs,
+    include_dirs=include_dirs))
+
 setup(name='uptide',
       version='0.1',
       author='Stephan Kramer',
       author_email='s.kramer@imperial.ac.uk',
       url='http://www.opentidalfarm.com',
-      packages = ['uptide'] 
+      packages = ['uptide'],
+      script_args = script_args,
+      ext_package = 'uptide',
+      ext_modules = ext_modules
       )
