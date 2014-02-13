231be2d0ce704f2b72973260900b87a74c9adaac
Stephan Kramer <s.kramer@imperial.ac.uk>
2014-02-13 17:36:30 +0000

    Add a regression test based on OpenTidalFarm example orkney_resource_assessment.
    This is a first of a set of regression tests that is meant to test
    whether we're changing anything in real life applications. They may
    depend on external data (in this case OTPS netcdf files) and are
    therefore not part of the standard tests.

---
+++ regression/extract_boundary_points.py
@@ -0,0 +1,34 @@
+import numpy
+import sys
+
+xy_file_name = sys.argv[1]
+msh_file_name = sys.argv[2]
+surface_ids = [int(x) for x in sys.argv[3:]]
+
+f = file(msh_file_name, 'r')
+
+while f.readline().strip() != '$Nodes':
+    pass
+
+nnodes = int(f.readline())
+xy = numpy.zeros((nnodes,2))
+for i in range(nnodes):
+    line = f.readline().strip().split(' ')
+    xy[int(line[0])-1,:] = [float(line[1]), float(line[2])]
+
+f.readline() # $EndNodes
+f.readline() # $Elements
+nelements =  int(f.readline())
+boundary_nodes = set()
+for i in range(nelements):
+    line = f.readline().strip().split(' ')
+    elm_type = int(line[1])
+    if int(line[1]) != 1:
+        continue
+    sid = int(line[3])
+    if sid in surface_ids:
+        boundary_nodes.add(int(line[4])-1)
+        boundary_nodes.add(int(line[5])-1)
+
+f.close()
+numpy.savetxt(xy_file_name, xy[list(boundary_nodes),:])
---
+++ regression/test_orkney.py
@@ -0,0 +1,49 @@
+import uptide
+import uptide.tidal_netcdf
+import os.path
+import datetime
+import numpy
+import utm
+import sys
+
+utm_zone = 30
+utm_band = 'V'
+ranges = ((-4.0,0.0), (58.0,61.0))
+otps_data_dir = '/home/skramer/git/OpenTidalFarm/examples/orkney_resource_assessment'
+grid_file_name = os.path.join(otps_data_dir, 'gridES2008.nc')
+data_file_name = os.path.join(otps_data_dir, 'hf.ES2008.nc')
+initial_time = datetime.datetime(2001, 9, 18, 0)
+constituents = ['Q1', 'O1', 'P1', 'K1', 'N2', 'M2', 'S2', 'K2']
+
+reference_file_name = 'orkney.dat'
+
+times = (0., 100.)
+boundary_xy = numpy.loadtxt('orkney.xy')
+
+tide = uptide.Tides(constituents)
+tide.set_initial_time(initial_time)
+tnci = uptide.tidal_netcdf.OTPSncTidalInterpolator(tide,
+            grid_file_name, data_file_name, ranges)
+
+vals_t = []
+for t in times:
+    tnci.set_time(t)
+    vals = []
+    for xy in boundary_xy:
+        latlon = utm.to_latlon(xy[0], xy[1], utm_zone, utm_band)
+        vals.append(tnci.get_val((latlon[1], latlon[0]), allow_extrapolation=True))
+
+    vals_t.append(vals)
+
+vals_t = numpy.array(vals_t).T
+# uncomment to update reference:
+#numpy.savetxt(reference_file_name, vals_t)
+
+reference = numpy.loadtxt(reference_file_name)
+err =  numpy.abs(reference-vals_t).max()
+print "err = ", err
+if err>1e-3:
+    print "Error too large"
+    sys.exit(1)
+else:
+    print "Success!"
