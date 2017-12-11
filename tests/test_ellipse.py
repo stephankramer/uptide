c3a5d298c78d817b0dd529e666072abd08eda2d8
Stephan Kramer <s.kramer@imperial.ac.uk>
2017-12-11 16:05:56 +0000

    tidal_ellipse_parameters() to also return phase
    The phase is wrt to the principal direction. The direction of this
    principal direction (already existing 3rd return value) may have changed
    by pi in some cases.

--- tests/test_ellipse.py
+++ tests/test_ellipse.py
@@ -5,21 +5,22 @@ import math
 class TestEllipse(unittest.TestCase):
   def test_tidal_ellipse_parameters(self):
     # first let's stick in some random numbers:
-    a,b,theta = ue.tidal_ellipse_parameters(1.0, 2.0, 3.0, 4.0)
-    for x,y in zip((a,b,theta),(3.0315505539181253, 0.8998340063804587, -1.4195315055223143)):
+    a,b,theta,pha = ue.tidal_ellipse_parameters(1.0, 2.0, 3.0, 4.0)
+    for x,y in zip((a,b,theta,pha),(3.0315505539181253, 0.8998340063804587, -1.4195315055223143,0.90362100129612022)):
       self.assertAlmostEqual(x,y)
     # if u and v are in phase, the ellipse should be flat
     # and in the direction of the combined components
-    a,b,theta = ue.tidal_ellipse_parameters(3.0,math.pi/2,4.0,math.pi/2)
-    for x,y in zip((a,b,theta),(5.0, 0.0, math.atan2(4.0,3.0))):
+    a,b,theta,pha = ue.tidal_ellipse_parameters(3.0,math.pi/2,4.0,math.pi/2)
+    for x,y in zip((a,b,theta,pha),(5.0, 0.0, math.atan2(4.0,3.0),math.pi/2)):
       self.assertAlmostEqual(x,y)
     # if u and v are in anti-phase, the ellipse should have au and av
     # as its minor and major radii and the ellipse should be cartesian aligned
-    a,b,theta = ue.tidal_ellipse_parameters(1.0, 1.2, 2.0, 1.2+math.pi/2.)
-    for x,y in zip((a,b,math.cos(theta)),(2.0,1.0,0.0)):
+    # because the major component is second (v-component) the principle axis flips to (0,-1)
+    a,b,theta,pha = ue.tidal_ellipse_parameters(1.0, 1.2, 2.0, 1.2+math.pi/2.)
+    for x,y in zip((a,b,math.cos(theta),math.cos(pha)),(2.0,1.0,0.0,-math.cos(1.2+math.pi/2.))):
       self.assertAlmostEqual(x,y)
-    a,b,theta = ue.tidal_ellipse_parameters(7.0, -math.pi*2/3., 1.0, -math.pi/6.)
-    for x,y in zip((a,b,math.sin(theta)),(7.0,1.0,0.0)):
+    a,b,theta,pha = ue.tidal_ellipse_parameters(7.0, -math.pi*2/3., 1.0, -math.pi/6.)
+    for x,y in zip((a,b,math.sin(theta),math.sin(pha)),(7.0,1.0,0.0,math.sin(-math.pi*2/3))):
       self.assertAlmostEqual(x,y)
 
 if __name__ == '__main__':
--- uptide/ellipse.py
+++ uptide/ellipse.py
@@ -10,15 +10,17 @@ def compute_focus_squared(au,pu,av,pv):
   return numpy.sqrt(au**4+av**4+2*au**2*av**2*numpy.cos(2*(pu-pv)))
 
 def tidal_ellipse_parameters(au,pu,av,pv):
-  """Computes the length, width and radius of a tidal ellipse
-  from the amplitude and phase of the x and y components of velocity.
+  """Computes the major and minor radii, direction of, and phase wrt
+  principle axis from the amplitude and phase of the u and v components of velocity.
   Accepts numpy arrays to compute parameters for multiple consituents
   at once."""
   f2 = compute_focus_squared(au,pu,av,pv)
   a = numpy.sqrt(0.5*(au**2+av**2+f2))
   b = numpy.sqrt(0.5*(au**2+av**2-f2))
-  # formula A3:4a from Pugh
-  delta = 0.5*numpy.arctan2(av**2*numpy.sin(2*(pu-pv)),au**2+av**2*numpy.cos(2*(pu-pv)))
-  # formula A3:8 from Pugh
-  theta = numpy.arctan2(av*numpy.cos(pu-pv-delta), au*numpy.cos(delta))
-  return a, b, theta
+
+  # from Pugh (A3:10)
+  gc = numpy.arctan2(au*numpy.sin(pu)+av*numpy.cos(pv), au*numpy.cos(pu)-av*numpy.sin(pv))
+  gac = numpy.arctan2(-au*numpy.sin(pu)+av*numpy.cos(pv), au*numpy.cos(pu)+av*numpy.sin(pv))
+  theta = 0.5 * (gc+gac)
+  g = 0.5 * (gc-gac) % (2*numpy.pi)
+  return a, b, theta, g
