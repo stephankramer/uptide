8c78091418c57bdfbd7a52cdbf8d6d77c7ae7db7
Stephan Kramer <s.kramer@imperial.ac.uk>
2019-10-23 18:00:44 +0100

    Add TPXO test

--- run_tidal_netcdf_test.sh
+++ run_tidal_netcdf_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 export OTPS_GRID_FILE='/home/skramer/data/otps/ES2008/gridES2008.nc'
 export OTPS_DATA_FILE='/home/skramer/data/otps/ES2008/hf.ES2008.nc'
+export TPXO_GRID_FILE='/home/skramer/data/tpxo9/grid_tpxo9.nc'
+export TPXO_DATA_FILE='/home/skramer/data/tpxo9/h_tpxo9.v1.nc'
 export FES2014_INI_FILE='/home/skramer/data/fes2014/ocean_tide_fixed.ini'
 export FES2014_DATA_PATH='/home/skramer/data/fes2014/ocean_tide/'
-python -mpytest -svx tests/test_tidal_netcdf.py
+python -mpytest --pdb -svx tests/test_tidal_netcdf.py
+
