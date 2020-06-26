fab8ca942860e20aa11b408c23fde56999d2a59a
Stephan Kramer <s.kramer@imperial.ac.uk>
2020-06-26 16:40:45 +0100

    Version 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 script_args = sys.argv[1:]
 
 setup(name='uptide',
-      version='0.5',
+      version='1.0',
       author='Stephan Kramer',
       author_email='s.kramer@imperial.ac.uk',
       description="uptide is a python package for tidal calculations. It computes the tidal " +
